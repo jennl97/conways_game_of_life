59177502ad46ebbc1840c2f4a921530b5ca4a3d4
Jennifer Hott-Leitsch <jenniferhott-leitsch@Mac-mini.hsd1.il.comcast.net>
2020-07-12 20:05:50 -0500

    Rearranged the layout, updated fonts and some colors

--- game_of_life/src/App.js
+++ game_of_life/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Functions from './logic/Functions';
+import About from './Components/About';
+
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      size:[0, 0],
+      size:[25, 25],
       generation: 0,
       gameRunning: false,
       functions: new Functions(),
@@ -156,7 +158,7 @@ class App extends Component {
           <label className="label">
               Generation: {this.state.functions.getGeneration()}
           </label>
-         <label>
+         <label className="label">
               Interval Speed: 
               <input className="input" value={this.state.interval} onChange={this.userInterval} />
          </label>
@@ -165,41 +167,9 @@ class App extends Component {
             {this.renderBoard()}
           </div>
         </div>
-        <div className="aboutContainer">
-          <h3>About:</h3>
-          <p>
-          The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
-          It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input.
-          One interacts with the Game of Life by creating an initial configuration and observing how it evolves.
-          It is Turing complete and can simulate a universal constructor or any other Turing machine.</p>
-          <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns" target="_blank" rel="noopener noreferrer">Wikipedia Source</a>
-          <h3>What Are The Rules?</h3>
-          <p>
-          The universe of the Game of Life is an infinite, two-dimensional orthogonal grid of square cells, each of which is in one of two possible states, live or dead, (or populated and unpopulated, respectively). 
-          Every cell interacts with its eight neighbours, which are the cells that are horizontally, vertically, or diagonally adjacent. 
-          <br/>
-          At each step in time, the following transitions occur:
-          </p>
-          <ol>
-          <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
-          <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
-          <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
-          <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
-          </ol>
-          <p>
-          These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
-          </p>
-          <ol>
-            <li>Any live cell with two or three live neighbours survives.</li>
-            <li>Any dead cell with three live neighbours becomes a live cell.</li>
-            <li>All other live cells die in the next generation. Similarly, all other dead cells stay dead.</li>
-          </ol>
-          <p>
-          The initial pattern constitutes the seed of the system.
-          The first generation is created by applying the above rules simultaneously to every cell in the seed; births and deaths occur simultaneously, and the discrete moment at which this happens is sometimes called a tick.
-          Each generation is a pure function of the preceding one. The rules continue to be applied repeatedly to create further generations.
-          </p>
-          </div>
+        <div className="mainAboutContainer">
+        <About />
+        </div>
       </div>
     </div>
   );
